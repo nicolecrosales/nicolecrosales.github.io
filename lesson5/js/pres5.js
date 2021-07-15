2ed2c034da16eb1ddbead160d2b081034493cbdd
nicolecrosales.github.io <nicole.carrascorosales@gmail.com>
2021-07-14 20:49:44 -0400

    weekfive

--- lesson5/js/pres5.js
+++ lesson5/js/pres5.js
@@ -1,6 +1,9 @@
 function toggleMenu () {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
-const date = new Date();
-document.querySelector("#currentyear").innerHTML = date.getFullYear();
-document.querySelector("#display").innerHTML = document.lastModified;
+
+const banner = document.querySelector('.banner');
+const dayOfWeek = new Date().getDay();
+if (dayOfWeek === 5) {
+    banner.classList.add('visible');
+}

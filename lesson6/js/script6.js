002f86806c20e73b51a46dc16662812b7340dc4b
nicolecrosales.github.io <nicole.carrascorosales@gmail.com>
2021-07-16 12:40:34 -0400

    new

---
+++ lesson6/js/script6.js
@@ -0,0 +1,13 @@
+/*-----------------------Menu Toggle------------------------------------- */
+function toggleMenu () {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+/*-------------------------Alert Friday--------------------------------------- */
+const banner = document.querySelector('.alert-friday');
+const dayOfWeek = new Date().getDay();
+if (dayOfWeek === 5) {
+    banner.classList.add('visible');
+}
+/*-----------------------------------Font--------------------------------------- */
+WebFont.load({google: {families: ["IBM Plex Serif", "Montserrat"]
+    }});

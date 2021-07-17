50e86f8535b8b5b2cdb1ea2d89f3195c75307e6b
nicolecrosales.github.io <nicole.carrascorosales@gmail.com>
2021-07-17 18:28:14 -0400

    weekten

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,14 @@
-let t = document.getElementById('temperature').textContent;
-let s = document.getElementById('speed').textContent;
+let t = document.getElementById('temp').textContent;
+let s = document.getElementById('windspeed').textContent;
 
 if(t <= 50 && s >= 3){
   let p = Math.pow(s, 0.16);
   let f = 35.74 + 0.6215 * t - 35.75 * p + 0.4275 * t * p;
   let result = f.toFixed(0);
-  document.getElementById('windChill').innerHTML = result + " °F";
+  document.getElementById('windchill').innerHTML = result + " °F";
 }
 else{
-   document.getElementById('windChill').textContent = "N/A";
+   document.getElementById('windchill').textContent = "N/A";
 }
 
 

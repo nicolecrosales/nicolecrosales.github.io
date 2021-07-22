ab045774ee8fd200fbea28248cfd9a84fd43019a
nicolecrosales.github.io <nicole.carrascorosales@gmail.com>
2021-07-22 16:04:00 -0400

    new

--- FinalProject/js/script.js
+++ FinalProject/js/script.js
@@ -19,7 +19,7 @@ document.querySelector("#date").textContent =  current;
 /*-----------------------Last Modified------------------------------------*/
 document.querySelector("#display").innerHTML = document.lastModified;
 /*----------------------------------local home-------------------------------*/
-const requestURL = '';
+const requestURL = 'https://nicolecrosales.github.io/FinalProject/directory.json';
 
     fetch(requestURL)
         .then(function (response) {
@@ -33,27 +33,27 @@ const requestURL = '';
                 let card = document.createElement('section');
                 
                 let h2 = document.createElement('h2');
-                let motto = document.createElement('h3');
-                let founded = document.createElement('p');
-                let population = document.createElement('p');
-                let rain = document.createElement('p');
-                let image = document.createElement('img');
+                let logo = document.createElement('img');
+                let num = document.createElement('p');
+                let web = document.createElement('p');
+                let add = document.createElement('p');
+                
         
                 h2.innerHTML = `${town[i].name}`;
-                motto.innerHTML = `${town[i].motto}`;
-                founded.innerHTML = 'Year Founded: ' + `${town[i].yearFounded}`;
-                population.innerHTML = 'Population: ' + `${town[i].currentPopulation}`;
-                rain.innerHTML = 'Annual Rain Fall: ' + `${town[i].averageRainfall}`;
+                logo.setAttribute('src', `Images/${town[i].photo}`);
+                logo.setAttribute('alt', ` ${town[i].name}`);
+                num.innerHTML = 'Number: ' + `${town[i].number}`;
+                web.innerHTML = 'Website: ' + `${town[i].website}`;
+                add.innerHTML = `${town[i].address}`;
     
-                image.setAttribute('src', `image/${town[i].photo}`);
-                image.setAttribute('alt', ` ${town[i].name}!`);
+                
     
                     card.appendChild(h2);
-                    card.appendChild(motto);
-                    card.appendChild(founded);
-                    card.appendChild(population);
-                    card.appendChild(rain);
-                    card.appendChild(image);
+                    card.appendChild(logo);
+                    card.appendChild(num);
+                    card.appendChild(web);
+                    card.appendChild(add);
+                    
                     document.querySelector('.home-local').appendChild(card);
                 }
         

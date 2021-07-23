ff401f39de069ef25004487ff40ce923c63d5c69
nicolecrosales.github.io <nicole.carrascorosales@gmail.com>
2021-07-23 09:28:11 -0400

    new

---
+++ FinalProject/js/directorey2.js
@@ -0,0 +1,39 @@
+const requestURL1 = 'https://nicolecrosales.github.io/FinalProject/directory.json';
+
+    fetch(requestURL1)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (jsonObject) {
+            const towns = jsonObject['business'];
+            const town = towns.filter(x => x.name == 'Mall Marina' || x.name == 'Vida Natura' || x.name == 'Paseo Viña Centro'  || x.name == 'Reitz Propiedades' || x.name == 'Decathlon' || x.name == 'PUMA' || x.name == 'Barbizon' || x.name == 'Belda Cycles' || x.name == 'BP Iluminación')
+    
+            for (let i = 0; i < town.length; i++) {
+                let card = document.createElement('section');
+                
+                let h2 = document.createElement('h2');
+                let logo1 = document.createElement('img');
+                let num1 = document.createElement('p');
+                let web1 = document.createElement('p');
+                let add1 = document.createElement('p');
+                
+        
+                h2.innerHTML = `${town[i].name}`;
+                logo1.setAttribute('src', `Images/${town[i].photo}`);
+                logo1.setAttribute('alt', ` ${town[i].name}`);
+                num1.innerHTML = 'Number: ' + `${town[i].number}`;
+                web1.innerHTML = 'Website: ' + `${town[i].website}`;
+                add1.innerHTML = `${town1[i].address}`;
+    
+                
+    
+                    card.appendChild(h2);
+                    card.appendChild(logo1);
+                    card.appendChild(num1);
+                    card.appendChild(web1);
+                    card.appendChild(add1);
+                    
+                    document.querySelector('.home-local2').appendChild(card);
+                }
+        
+        });
--- FinalProject/js/directory.js
+++ FinalProject/js/directory.js
@@ -1,12 +1,12 @@
-const requestURL1 = 'https://nicolecrosales.github.io/FinalProject/workers.json';
+const requestURL1 = 'https://nicolecrosales.github.io/FinalProject/directory.json';
 
     fetch(requestURL1)
         .then(function (response) {
             return response.json();
         })
         .then(function (jsonObject) {
-            const towns1 = jsonObject['workers'];
-            const town1 = towns1.filter(x => x.name == 'General Director' || x.name == 'Sub Director' || x.name == 'Executive Director' || x.name == 'Director')
+            const towns1 = jsonObject['business'];
+            const town1 = towns1.filter(x => x.name == 'Mall Marina' || x.name == 'Vida Natura' || x.name == 'Paseo Viña Centro'  || x.name == 'Reitz Propiedades' || x.name == 'Decathlon' || x.name == 'PUMA' || x.name == 'Barbizon' || x.name == 'Belda Cycles' || x.name == 'BP Iluminación')
     
             for (let i = 0; i < town1.length; i++) {
                 let card1 = document.createElement('section');
@@ -21,20 +21,19 @@ const requestURL1 = 'https://nicolecrosales.github.io/FinalProject/workers.json'
                 h21.innerHTML = `${town1[i].name}`;
                 logo.setAttribute('src', `Images/${town1[i].photo}`);
                 logo.setAttribute('alt', ` ${town1[i].name}`);
-                num.innerHTML = 'Name: ' + `${town1[i].namep}`;
-                web.innerHTML = 'Section: ' + `${town1[i].area}`;
-                add.innerHTML = 'Contact: ' + `${town1[i].contact}`;
+                num.innerHTML = 'Number: ' + `${town1[i].number}`;
+                web.innerHTML = 'Website: ' + `${town1[i].website}`;
+                add.innerHTML = `${town1[i].address}`;
     
                 
     
-                card1.appendChild(logo);
-                card1.appendChild(h21);
-                    
+                    card1.appendChild(h21);
+                    card1.appendChild(logo);
                     card1.appendChild(num);
                     card1.appendChild(web);
                     card1.appendChild(add);
                     
-                    document.querySelector('.dic').appendChild(card1);
+                    document.querySelector('.home-local').appendChild(card1);
                 }
         
         });
